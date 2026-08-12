04eabd8e079a413b156faaaab4800545770db3f9
Your Name <you@example.com>
2026-08-12 21:14:14 +0200

    Úprava do Angličtiny

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ const projectsData = {
     'web-1': {
         type: 'Web App',
         name: 'Splitsound',
-        description: 'Webová aplikace s umělou inteligencí, která převádí zvukové soubory (MP3) do notového zápisu. Automaticky rozděluje skladby na jednotlivé nástrojové stopy a generuje interaktivní notový zápis.<br><br><strong>Technologie:</strong> Figma, React, JavaScript, Demucs, Music21, OSMD',
+        description: 'An AI-powered web application that converts audio files (MP3) into sheet music. It automatically splits songs into individual instrumental tracks and generates interactive sheet music. <br><br><strong>Technology:</strong> Figma, React, JavaScript, Demucs, Music21, OSMD',
         image: 'icons/projects/splitsound-project.png', 
         link: 'https://github.com/Smelda07/Splitsound'
     },
     'web-2': {
         type: 'Mobile App',
         name: 'Musicom',
-        description: 'Moderní mobilní aplikace jako sociální síť pro hudebníky postavená v React Native, Expo a Appwrite backendu. <br><br><strong>Technologie:</strong> Figma, Tailwind, React native, Expo, JavaScript',
+        description: 'A modern mobile app serving as a social network for musicians, built using React Native, Expo, and the Appwrite backend. <br><br><strong>Technology:</strong> Figma, Tailwind, React native, Expo, JavaScript',
         image: 'icons/projects/musicom-project.png',
         link: 'https://github.com/Smelda07/Musicom-V2'
     },
     'app-1': {
         type: 'Web',
         name: 'A1-Makers',
-        description: 'Prezentační a firemní webové stránky pro A1-MAKERS s.r.o. s podporou 3 jazyků a Google Maps API. <br><br><strong>Technologie:</strong> HTML, Tailwind, JavaScript, Google Maps API',
+        description: 'A corporate and promotional website for A1-MAKERS s.r.o. with support for three languages and the Google Maps API. <br><br><strong>Technology:</strong> HTML, Tailwind, JavaScript, Google Maps API',
         image: 'icons/projects/a1-makers-project.png',
         link: 'https://github.com/Smelda07/A1-Makers'
     },
     'app-2': {
         type: 'Web',
         name: 'Restaurant',
-        description: 'Moderní, plně responsivní a dvoujazyčný (CZ/DE) web pro penzion a restauraci Zátiší v Rumburku. Vytvořeno pomocí HTML5, Tailwind CSS a JavaScriptu. (doplň vlastní popis).<br><br><strong>Technologie:</strong> HTML, Tailwind, JavaScript',
+        description: 'A modern, fully responsive, and bilingual (CZ/DE) website for the Zátiší guesthouse and restaurant in Rumburk. Built using HTML5, Tailwind CSS, and JavaScript. <br><br><strong>Technology:</strong> HTML, Tailwind, JavaScript',
         image: 'icons/projects/restaurant-project.png',
         link: 'https://github.com/Smelda07/Restaurant-Zatisi'
     }
